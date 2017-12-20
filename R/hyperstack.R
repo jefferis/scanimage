303cee6a5fa669f466221800b0c5e35ea43b08cd
Gregory Jefferis <jefferis@gmail.com>
2017-12-20 22:43:32 +0000

    Pass on ... in make_hyperstack

--- R/hyperstack.R
+++ R/hyperstack.R
@@ -11,8 +11,8 @@
 #' @return a 4D array typically ordered x,y,z,t or x,y,t,file
 #' @export
 #' @importFrom abind abind
-make_hyperstack<-function(x, zslices, timepoints, time.axis=4){
-  if(is.character(x)) x=read.any.tiff(x)
+make_hyperstack<-function(x, zslices, timepoints, time.axis=4, ...){
+  if(is.character(x)) x=read.any.tiff(x, ...)
   if(missing(zslices)) zslices=length(x)/timepoints
   if(missing(timepoints)) timepoints=length(x)/zslices
   if(round(zslices*timepoints)!=length(x))
@@ -30,11 +30,13 @@ make_hyperstack<-function(x, zslices, timepoints, time.axis=4){
 #'   which will have 2-3 dimensions (usually x,y,t) and combines into a single
 #'   hyperstack x,y,t,file
 #' @param file.axis Which axis will end up as the file axis in the output when
-#'   reading multiple files.
+#'   reading multiple files
+#' @param ... Additional arguments passed to \code{make_hyperstack}, thence to
+#'   \code{\link{read.any.tiff}} and eventually to \code{\link{read.scanimage}}.
 #' @export
 #' @rdname make_hyperstack
 #' @importFrom abind abind
-make_hyperstack_multifile <- function(x, file.axis=4) {
+make_hyperstack_multifile <- function(x, file.axis=4, ...) {
   ll=lapply(x, make_hyperstack, zslices=1, time.axis=setdiff(3:4, file.axis))
   abind(ll, along=file.axis)
 }
