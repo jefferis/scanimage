848712f0b730d9b665338590a284a020fdaf559b
Gregory Jefferis <jefferis@gmail.com>
2017-12-21 20:40:57 +0000

    keep scanimage attributes when making hyperstack
    * we can use these later to get useful info like framerate

--- R/hyperstack.R
+++ R/hyperstack.R
@@ -3,12 +3,17 @@
 #' @description \code{make_hyperstack} reads a single TIFF which may have 3-4
 #'   dimensions.
 #'
+#' @details Where possible the hyperstack will include the \code{description}
+#'   attribute from the original scanimage file. This can be used with
+#'   \code{\link{scanimageinfo}} or \code{\link{parse_description}} to access
+#'   useful information about the underlying image.
+#'
 #' @param x Path to (one or more) TIFFs
 #' @param zslices Number of zlices in each TIFF
 #' @param timepoints Number of timepoints in each TIFF
 #' @param time.axis Which axis will end up as the time axis in the output
 #'
-#' @return a 4D array typically ordered x,y,z,t or x,y,t,file
+#' @return a 4D array typically ordered x,y,z,t or x,y,t,file.
 #' @export
 #' @importFrom abind abind
 make_hyperstack<-function(x, zslices, timepoints, time.axis=4, ...){
@@ -23,7 +28,9 @@ make_hyperstack<-function(x, zslices, timepoints, time.axis=4, ...){
     idxs=seq(from=zslices*(i-1)+1,length.out = zslices)
     timepoint.stacks[[i]]=abind(x[idxs], along = 3)
   }
-  abind(timepoint.stacks, along = time.axis)
+  res=abind(timepoint.stacks, along = time.axis)
+  attr(res, 'description')=attr(x[[1]], 'description')
+  res
 }
 
 #' @description \code{make_hyperstack_multifile} reads multiple TIFFs each of
@@ -38,5 +45,7 @@ make_hyperstack<-function(x, zslices, timepoints, time.axis=4, ...){
 #' @importFrom abind abind
 make_hyperstack_multifile <- function(x, file.axis=4, ...) {
   ll=lapply(x, make_hyperstack, zslices=1, time.axis=setdiff(3:4, file.axis), ...)
-  abind(ll, along=file.axis)
+  res=abind(ll, along=file.axis)
+  attr(res, 'description')=attr(ll[[1]], 'description')
+  res
 }
